e795b99bd01511fd18cdd48c504b32f4a930a6b8
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-11-06 20:37:10 +0100

    new changes 06.11

--- assigmentMVC/Controllers/GuessController.cs
+++ assigmentMVC/Controllers/GuessController.cs
@@ -5,16 +5,52 @@ namespace assigmentMVC.Controllers
     public class GuessController : Controller
         
     {
+        Random random = new();
+
         [HttpPost]
-        public IActionResult GuessNumber(Random number)
+        public IActionResult GuessNumber(int guess)
         {
-            ViewBag.Number = Models.Guess.RandomNumbers(number);
+            string? randomNr = Request.Cookies["random"];
+            int rand;
+            if (randomNr == null)
+            {
+                //add cookie
+                CookieOptions options = new CookieOptions();
+                options.Expires = DateTime.Now.AddMinutes(10);
+
+                int r = random.Next(0, 100);
+                Response.Cookies.Append("random", r.ToString(), options);
+                rand = r;
+            }
+            else
+            {
+                rand = int.Parse(randomNr);
+            }
+            ViewBag.Number = Models.Guess.RandomNumbers(guess, rand);
             return View();
         }
+
+
+
         [HttpGet]
         public IActionResult GuessNumber()
         {
+            string? randomNr = Request.Cookies["random"];
+
+            if (randomNr == null)
+            {
+                //remove cookie
+                CookieOptions option = new CookieOptions();
+                option.Expires = DateTime.Now.AddDays(-1);
+
+                random.Next(0, 100);
+                Response.Cookies.Append("random", "", option);
+            }
             return View();
         }
+
     }
 }
+
+
+
--- assigmentMVC/Models/Guess.cs
+++ assigmentMVC/Models/Guess.cs
@@ -2,7 +2,32 @@
 {
     public class Guess
     {
-         
-        
+        static int guesses = 0;
+        public static string RandomNumbers(int guess, int rand)
+        {
+            while (guesses != 10 || rand == guess)
+            {
+                if (guess < rand)
+                {
+                    guesses++;
+                    return "Guess number: " + guess + " To small. You have guessed: " + guesses + "times.";
+                }
+                else if (guess > rand)
+                {
+                    guesses++;
+                    return "The guessing number: " + guess + " was to big. You have guessed " + guesses + "times.";
+                }
+                else
+                {
+                    return guess + " was your guess and it was equals to the random number: " + rand + " and you have guesses " + guesses + " times";
+                }
+            }
+            return "No input.";
+
+        }
     }
-}
+}    
+       
+        
+    
+
